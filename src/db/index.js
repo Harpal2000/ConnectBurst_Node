9c07987b279e79af672e776a9fcfdd737656e079
Harpal7528 <rajanbabrah056@gmail.com>
2024-03-13 13:28:46 +0530

    connected to the database

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,32 @@
 import pg from "pg";
-
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "connectburst",
-  password: "developer@123",
-  port: 5433,
-});
+const db_config = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+const pool = new Pool(db_config);
 
 const connectDB = async () => {
   try {
     const client = await pool.connect();
-    console.log("\nPostgreSQL Connected to",`"`,client.database,`"`,"Database at PORT", client.port,"!!");
+    console.log(
+      "\nPostgreSQL Connected to",
+      `"`,
+      client.database,
+      `"`,
+      "Database at PORT",
+      client.port,
+      "!!"
+    );
   } catch (error) {
     console.log("PostgreSQL Connection Failed", error);
   }
