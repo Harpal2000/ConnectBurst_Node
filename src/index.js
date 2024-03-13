4c32f618212f22820a690ac0772b138cd3b2ee99
Harpal7528 <rajanbabrah056@gmail.com>
2024-03-13 13:35:35 +0530

    make express app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import express from "express";
 import connectDB from "./db/index.js";
-import dotenv from "dotenv"
+import { app } from "./app.js";
+import dotenv from "dotenv";
 dotenv.config({
-    path: './.env'
-})
-
-const app = express();
+  path: "./.env",
+});
 
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8800, () => {
-      console.log(`Server is running at port : ${process.env.PORT}`);
+      console.log(`Server is running at PORT ${process.env.PORT}`);
     });
   })
   .catch((err) => {
-    console.log("PostgreSQL connection failed !!! ", err);
+    console.log("PostgreSQL connection failed !! ", err);
   });
