9c3319171c8043177de94af1f394e7bf00d8ddba
Harpal7528 <rajanbabrah056@gmail.com>
2024-04-02 12:49:04 +0530

    add error handler

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 
-app.use("/", routes);
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoutes);
 
 export { app };
